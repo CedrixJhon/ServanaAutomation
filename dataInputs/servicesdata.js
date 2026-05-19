bf64a853d3f99a4a4afa071e4571fb7993070a72
Cedrix Jhon Busa <cedrix.busa@gmail.com>
2026-05-19 20:18:06 +0800

    Added codes on Services section

--- dataInputs/servicesdata.js
+++ dataInputs/servicesdata.js
@@ -6,4 +6,8 @@ const category='Test Category';
 const option1Name='Test Option 1';
 const itemName='Test Item';
 const basePrice='100';
-module.exports={serviceName,category,option1Name,itemName,basePrice};
+const inclusions='Test Inclusion 0111'
+const exclusions='Test Exclusion 0222';
+
+
+module.exports={serviceName,category,option1Name,itemName,basePrice, inclusions,exclusions};
